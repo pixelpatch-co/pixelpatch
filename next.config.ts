2c60c7b3cb3d8b02ec09a335b7c9157fa16a9560
pixelpatch-co <contact.pixelpatch@gmail.com>
2025-08-24 21:51:05 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
 import type {NextConfig} from 'next';
 
-const isProd = process.env.NODE_ENV === 'production';
-// IMPORTANT: CHANGE THIS TO YOUR GITHUB REPOSITORY NAME
 const repoName = 'pixelpatch'; 
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   typescript: {
     ignoreBuildErrors: true,
   },
